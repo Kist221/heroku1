65917a020a490b2cd505e365994f82dcbef7c4b3
Kist221 <Kist221@gmail.com>
2018-02-01 21:04:26 -0700

    message

--- server.js
+++ server.js
@@ -92,5 +92,5 @@ app.post("/api/new", function(req, res) {
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
